84f11566a04c3b035d85a4a72aefc479da093af8
Munsterberg <jetrussell93@gmail.com>
2016-09-17 17:15:26 -0400

    write basic sanity tests

---
+++ test/App.spec.js
@@ -0,0 +1,27 @@
+/* eslint-disable */
+
+import app from '../src/app';
+import {expect} from 'chai';
+import request from 'supertest';
+
+describe('404 on nonexistent URL', () => {
+  it('should return a 404 no url', (done) => {
+    request(app)
+      .get('/FailNoUrl')
+      .expect(404)
+      .expect('Content-Type', 'text/html; charset=utf-8', done)
+  });
+});
+
+describe('GET /', () => {
+  it('should return Hola', (done) => {
+    request(app)
+      .get('/')
+      .expect(200)
+      .expect('Content-Type', 'text/html; charset=utf-8')
+      .end((err, res) => {
+        expect(res.text).to.equal('Hola')
+        done();
+      });
+  });
+});
