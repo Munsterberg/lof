d7d7c5e72e299e76a3add24eed848332cab9beb9
Munsterberg <jetrussell93@gmail.com>
2016-09-19 15:44:02 -0400

    remove unnecessary if/else

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,14 +16,14 @@ router.route('/summoner')
     const summoner = req.body.summoner.toLowerCase().replace(/\s+/g, '');
     const summonerUrl = `https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/${summoner}?api_key=${apiKey}`; // eslint-disable-line
 
-    if(!summoner) console.log('No summoner found');
     request(summonerUrl, (error, response, body) => {
       if (!error && response.statusCode === 200) {
         const summonerInfo = JSON.parse(body);
         const summonerObj = summonerInfo[summoner];
         res.render('summoner/player', {summonerInfo: summonerObj, title: summonerObj.name});
       } else {
-        console.log(error);
+        console.log('A summoner was not found!');
+        res.redirect('/summoner');
       }
     });
   });
